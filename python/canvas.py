b7751f16c4a46bfafb153e90bedb1aac31ec0261
bviola <bruno.viola@ukaea.uk>
2019-02-14 16:22:21 +0000

    bug fix
    fixed bug when exiting and no data or status flag are changed
    
    now all experimental data is stored inside self.data defined as a namespace (a container)
    
    started writing documentation
    
    bug fix in logger
    
    deleted unused files
    
    developing mouse and keyboard events
    
    changes to canvas class to allow for mouse events call
    
    changes to main gui in qt design to allow for keyboard event when clicking on the manual correction QLineEdit
    
    new objects in scratch_67 that can be used to define keyboard events
    
    more development
    
    -canvas class supplied with logger
    -canvas class emits signal
    -connected signal emitted by canvas to store clicked datapoint
    -datapoint appended to list (one per channel)
    
    adding correction of fringe jump feature in class KG1PPfdata
    
    now plots fringe corrections for each channel as vertical lines
    
    single correction works
    it has been tested just for channel 1
    apparently there are still some cases not covered.
    
    bug fix to logger
    
    now displaying pulse number in Window name
     -when there is already data in the workspace
     -after reading a new pulse it is updated
    
     changed class SignalBase to add correct/uncorrect fj
    
     changed class KG1PPFdata
    
     now needs to be implemented keyboard event to activate corrections
    
    creating signals
    
    keyboard events have been created and now connected to proper actions
    
    needs testing
    and coding of the actions
    
    testing uncorrect fj
    
    undo correction works for channel 1 singles correction
    
    single correction for channel 1 works
    
    key event . to activate single correction mode
    choose data poin with right click on canvas
    chose correction on widget that will appear
    update canvas
    
    click undo button to restore to previous state
    
    now selected point is red
    
    fixing bugs when correcting single point for other channels.
    up to now ch1 and 5 are ok
    
    still have to resolve issue that red point (where correction is applied) stay on canvas
    
    need to implement correction for vibration
    
    more bug fixes
    
    now correction for lateral channels works
    still need to implement proper corrections for mirror movements
    
    now undo single correction
    undoes just one point selected (last one)
    
    single correction seems to work
    
    it is possible to save workspace and then reaload the newly modified data when pressing load button.
    
    too many debug plus messages when changing tabs!
    
    undo restores data
    
    multi correction ok
    
    what happens to wrong correction made previously?
    
    added correction to signal KG1
    
    used to display total number of corrections per channel
    this will contain info regarding intershot corrections (e.g. made by user)
    
    created new class to handle KG1 data
    
    density and vibration in particular have now attribute correction
    where intershot corrections will be stored
    
    bug fixes on logger
    
    bug fixes on readdata
    
    bug fix
    
    fixing display of total correction when chan is not integer
    
    now displays magenta vertical lines for user correction
    
    now undo_singlecorrection also remove the vertical magenta line added
    
    now multicorrection and undo correction are working
    
    the uncorrect_fj has been modified: now uses a custom function to search for the nearest point in the vector instead of isclose
    
    there are still bugs when going from single correction to multiple correction
    
    added profiles to code and pycallback
    
    following module are now working - multicorrection and singlecorrection
    undomulticorrection and unosingle correction
    
    still not working very well is the trainsition between two events.

--- python/canvas.py
+++ python/canvas.py
@@ -1,26 +1,51 @@
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
-# from matplotlib.backends.backend_gtkagg import NavigationToolbar2GTKAgg as NavigationToolbar
-
-from PyQt4 import QtGui
-
 # ----------------------------
 __author__ = "B. Viola"
 # ----------------------------
 
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.pyplot as plt
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4 import QtGui
+import logging
+logger = logging.getLogger(__name__)
+
 class Canvas(FigureCanvas):
+
     """
     CLASS used to convert widget into a matplotlib figure
+
+    contains mouse event (right click) that returns (xs,yx) when click is in axes
+
     """
-    
+
+    signal = pyqtSignal()
     def __init__(self, parent=None):
         self.figure = plt.figure()
         FigureCanvas.__init__(self, self.figure)
         FigureCanvas.setSizePolicy( self, QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Expanding )
         FigureCanvas.updateGeometry( self )
         self.setParent(parent)
+        self.xs = {}
+        self.ys = {}
+        self.cid = self.figure.canvas.mpl_connect('button_press_event', self)
 
+    def __call__(self, event):
+        if event.button == 3:  # right button
+            # print('click', event)
+            if event.inaxes is None:
+                pass
+            else:
+                self.xs = float(event.xdata)
+                self.ys = float(event.ydata)
+
+
+                logger.log(5, "(time, data) = ({},{})".format(self.xs,self.ys))
+            self.signal.emit()
+            return self.xs, self.ys
+
+        if event.button != 3:
+            pass
 
 
 
